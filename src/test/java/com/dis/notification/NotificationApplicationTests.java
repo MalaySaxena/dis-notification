2a21b922b17449ec5932f45a39aec5f91e0d9a2c
Malay Saxena <malay@Malays-Mac-mini.local>
2021-11-13 15:33:10 +0530

    first commit

---
+++ src/test/java/com/dis/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dis.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
